9791f8d7fb74da96bde49583a70c0ce5f5ef706f
seki kohei <sekikohei@sekinoMacBook-Pro.local>
2024-10-17 00:20:56 +0900

    10/16実施分

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -1,16 +1,36 @@
 package com.example.todo.controller.task;
 
 import com.example.todo.service.task.TaskService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.stream.Stream;
 
 @Controller
+@RequiredArgsConstructor
 public class TaskController {
-    private final TaskService taskService = new TaskService();
+    private final TaskService taskService;
+
     @GetMapping("/tasks")
     public String list(Model model){
-        model.addAttribute("taskList", taskService.find());
+        Stream<TaskDTO> taskList = taskService.find()//List<TaskEntity> -> List<TaskDTO>
+                .stream()
+                .map(TaskDTO::toDTO);
+
+        model.addAttribute("taskList", taskList);
         return "/tasks/list";
+    }
+
+    @GetMapping("/tasks/{id}")
+    public String showDetail(@PathVariable("id") long taskId, Model model){
+        //taskId->taskEntityを取得する
+        var taskEntity = taskService.findById(taskId)
+                .orElseThrow(() -> new IllegalArgumentException("Task not found: id =" + taskId));
+        model.addAttribute("task", TaskDTO.toDTO(taskEntity));
+        //戻り値は、htmlのファイル名
+        return "/tasks/detail";
     }
 }
--- src/main/java/com/example/todo/service/task/TaskService.java
+++ src/main/java/com/example/todo/service/task/TaskService.java
@@ -1,14 +1,21 @@
 package com.example.todo.service.task;
 
-import com.example.todo.controller.task.TaskDTO;
+import com.example.todo.repository.task.TaskRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class TaskService {
-    public List<TaskDTO> find() {
-        TaskDTO task1 = new TaskDTO(1L, "Spring Bootを学ぶ", "ToDoアプリケーションを作ってみる", "ToDO");
-        TaskDTO task2 = new TaskDTO(2L, "Spring Securityを学ぶ", "ログイン機能を作ってみる", "ToDO");
-        return  List.of(task1, task2);
+    private final TaskRepository taskRepository;
+    public List<TaskEntity> find() {
+        return taskRepository.select();
+    }
+
+    public Optional<TaskEntity> findById(long taskId) {
+        return taskRepository.selectById(taskId);
     }
 }
